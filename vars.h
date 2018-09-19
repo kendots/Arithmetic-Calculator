18529e419eac8044d96b4e7a2928fedac7d34a8c
kendots <kendotsaz@gmail.com>
2018-09-20 00:47:38 +0300

    Update vars.h

--- vars.h
+++ vars.h
@@ -2,7 +2,11 @@
 #define VARS_H
 
 #include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#define fun_num 14
 
+typedef double (*Fun) ();
 
 typedef struct var
 {
@@ -13,13 +17,76 @@ typedef struct var
 typedef struct vfun
 {
 	char name[20];
-	char va[5][20];
-	char fun[50];
+	Fun f;
 }vfun;
 
-var  * var_list;
-vfun * fun_list;
-int var_count=2, fun_count=0;
+
+vfun fun_list[fun_num];
+var * var_list;
+char (*logs)[256];
+int var_count=0, fun_count=0, line=0;
+
+
+void Init()
+{
+	logs=malloc(100*sizeof(char *));
+	var_list = malloc(100*sizeof(var));
+
+	strcpy(var_list[var_count].name,"ans");
+	var_list[var_count++].value=0;
+
+	strcpy(var_list[var_count].name,"pi");
+	var_list[var_count++].value=3.14159265359;
+
+	strcpy(fun_list[fun_count].name,"exp");
+	fun_list[fun_count++].f=exp;
+
+	strcpy(fun_list[fun_count].name,"ln");
+	fun_list[fun_count++].f=log;
+
+	strcpy(fun_list[fun_count].name,"log");
+	fun_list[fun_count++].f=log;
+
+	strcpy(fun_list[fun_count].name,"log10");
+	fun_list[fun_count++].f=log10;
+
+	strcpy(fun_list[fun_count].name,"sqrt");
+	fun_list[fun_count++].f=sqrt;
+
+	strcpy(fun_list[fun_count].name,"sin");
+	fun_list[fun_count++].f=sin;
+
+	strcpy(fun_list[fun_count].name,"cos");
+	fun_list[fun_count++].f=cos;
+
+	strcpy(fun_list[fun_count].name,"tan");
+	fun_list[fun_count++].f=tan;
+
+	strcpy(fun_list[fun_count].name,"asin");
+	fun_list[fun_count++].f=asin;
+
+	strcpy(fun_list[fun_count].name,"acos");
+	fun_list[fun_count++].f=acos;
+
+	strcpy(fun_list[fun_count].name,"atan");
+	fun_list[fun_count++].f=atan;
+
+	strcpy(fun_list[fun_count].name,"sinh");
+	fun_list[fun_count++].f=sinh;
+
+	strcpy(fun_list[fun_count].name,"cosh");
+	fun_list[fun_count++].f=cosh;
+
+	strcpy(fun_list[fun_count].name,"tanh");
+	fun_list[fun_count++].f=tanh;
+}
+
+
+void Clear()
+{
+	free(logs);
+	free(var_list);
+}
 
 
 #endif 
