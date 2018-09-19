fd0b1648475742c52aefc718d9183681f2015ed3
kendots <kendotsaz@gmail.com>
2018-09-20 00:48:28 +0300

    Update Calc.c

--- Calc.c
+++ Calc.c
@@ -7,24 +7,6 @@
 #define N 50
 #define MAX 250
 #define eps __DBL_EPSILON__
-char (*logs)[256];
-int line=0;
-
-
-#define ln_X   2000
-#define sin_X  3000
-#define cos_X  3001
-#define tan_X  3002
-#define exp_X  3003
-#define log_X  3004
-#define asin_X 4000
-#define acos_X 4001
-#define atan_X 4002
-#define sinh_X 4003
-#define cosh_X 4004
-#define tanh_X 4005
-#define sqrt_X 4006
-
 
 
 void oper (char []);
@@ -38,26 +20,45 @@ void unary (char []);
 int  scan (char [], char, char, int);
 int  scan2 (char [], char[], char, int);
 int  constants (char []);
-void func (char []);
-void funcs (char [], int );
+void func (char [], int, int, char []);
+void end (char []);
 int  fill (char [], char [], int, int);
+void insert (char [], char, int);
 int  get (char []);
 void handler (int);
+char num (char);
+char chara (char);
+char sign (char);
+char bra (char);
 void Init ();
 void Clear ();
 
-int main ()
+
+
+int main (int argc, char * argv[])
 {
 	char s[MAX];
-	printf("%s\n","KENDOTS CALCULATOR");
 	Init();
-	signal(SIGINT, handler);
-	while (1)
+	if (argc>1)
 	{
-		fputs(">> ",stdout);
-		get(s);
-		if (!strcmp(s,"exit")) break;
-		oper(s);
+		int i;
+		for (i=1; i<argc; i++)
+		{
+			strcpy(s,argv[i]);
+			oper(s);
+		}
+	}
+	else
+	{
+		printf("%s\n","KENDOTS CALCULATOR");
+		signal(SIGINT, handler);
+		while (1)
+		{
+			fputs(">> ",stdout);
+			get(s);
+			if (!strcmp(s,"exit")) break;
+			oper(s);
+		}
 	}
 	Clear();
 	return 0;
@@ -78,55 +79,15 @@ void oper (char s[])
 {
 	nospace(s);
 	if (assign(s))
-	{
-		if (calc(s)==0)
+		puts("Variable Assigned");
+	else
+	{	if (calc(s)==0)
 			return;
 		var_list[0].value=atof(s);
 		puts(s);
 		//int n=strlen(s);
 	}
-	else
-		puts("Variable Assigned");
-}
 
-
-int assign (char s[])
-{
-	int i,j,n;
-	n=strlen(s);
-	for (i=0; i<n; i++)
-		if (s[i]=='=') break;
-	if (i<n)
-	{
-		if (s[i-1]==')')
-			puts("Not done yet");
-		else
-		{
-			var a;
-			char str[N];
-			int k=0;
-			double x;
-			for (j=0; j<i; j++)
-				a.name[j]=s[j];
-			for (j=i+1; j<n; j++)
-				str[k++]=s[j];
-			str[k]='\0';
-			a.name[i]='\0';
-			//search for name
-			
-			calc(str);
-			x=atof(str);
-			a.value=x;
-			for (j=0; j<var_count; j++)
-				if (strcmp(a.name,var_list[j].name)==0)
-					break;
-			var_list[j]=a;
-			if (j==var_count)
-				var_count++;
-		}
-		return 0;
-	}
-	return 1;
 }
 
 
@@ -148,7 +109,44 @@ int calc (char s[])
 	unary(s);
 	solve(s,'-');
 	solve(s,'+');
+	end(s);
 	return 1;
+}
+
+
+//assigns a new variable from the user
+int assign (char s[])
+{
+	int i,j,n;
+	n=strlen(s);
+	for (i=0; i<n; i++)
+		if (s[i]=='=') break;
+	if (i<n)
+	{
+		var a;
+		char str[N];
+		int k=0;
+		double x;
+		for (j=0; j<i; j++)
+			a.name[j]=s[j];
+		for (j=i+1; j<n; j++)
+			str[k++]=s[j];
+		str[k]='\0';
+		a.name[i]='\0';
+		//search for name
+
+		calc(str);
+		x=atof(str);
+		a.value=x;
+		for (j=0; j<var_count; j++)
+			if (strcmp(a.name,var_list[j].name)==0)
+				break;
+		var_list[j]=a;
+		if (j==var_count)
+			var_count++;
+		return 1;
+	}
+	return 0;
 }
 
 
@@ -157,17 +155,15 @@ int constants (char s[])
 	int m,n,i,j,l,k=0,r;
 	char str[25];
 	n=strlen(s);
-	for (i=1; i<n-1; i++)
-		if (s[i]=='e' && s[i-1]>='0' && s[i-1]<='9' && ( (s[i+1]>='0' && s[i+1]<='9') || s[i+1]=='+' || s[i+1]=='-'))
-			fill(s,"*10^",i,i);
+
 	for (i=0; i<n; i++)
-		if (s[i]>='a' && s[i]<='z')
+		if (s[i]>='a' && s[i]<='z' && s[i]!='e')
 		{
 			j=i; k=0;
 			while (j<n)
 			{
 				str[k++]=s[j++];
-				if (s[j]=='+' || s[j]=='-' || s[j]=='*' || s[j]=='/' || s[j]=='^' || s[j]=='(') break;
+				if (sign(s[j]) || s[j]=='(' || s[j]==')') break;
 			}
 
 			str[k]='\0';
@@ -199,7 +195,8 @@ int constants (char s[])
 int bracket (char s[])
 {
 	char t[N],a[]="**";
-	int i,m,n,l,k,h;
+	int i,m,n,l,k,h,w;
+	double x;
 	l=strlen(s);
 	for (i=0; i<l; i++)
 	{
@@ -214,41 +211,49 @@ int bracket (char s[])
 		if (s[i]==')') m++;
 	}
 
-	for (i=1; i<l; i++)
-	{
-		if (s[i]=='(' && ( (s[i-1]>='0' && s[i-1]<='9') || s[i-1]==')' ) )
-		{
-			for (k=i-1; k>=0; k--)
-				
-			for (k=l; k>i; k--)
-				s[k]=s[k-1];
-			s[i]='*';
-			s[++l]='\0';
-		}
-	}
-
 	if (m-n)
 	{
 		puts("Error: Misplacement of bracket(s)");
 		return 0;
 	}
 
-	func(s);
-
 	m=scan(s,')',1,0);
 	while (m>0)
 	{
 		l=strlen(s);
-		for (i=0; i<m;i++)
-			t[i]=s[i];
-		t[m]='\0';
-		n=scan(t,'(',0,0);
+		n=scan(s,'(',0,m);
 
 		for (i=n+1; i<m; i++)
 			t[i-n-1]=s[i];
 		t[i-n-1]='\0';
 		if (s[m+1]=='^') s[m+1]='<';
 		calc(t);
+
+		if (n>0 && (num(s[n-1]) || chara(s[n-1])) )
+		{
+			w=n-2;
+			while (w>=0 && !sign(s[w]) && !bra(s[w]))
+				w--;
+			k=w+1;
+			do
+				w++;
+			while (num(s[w]));
+
+			if (w>k)
+			{
+				insert(s,'*',w);
+				m++;
+				n++;
+				w++;
+			}
+
+			if (w<n-1)
+			{
+				func(s,w,n-1,t);
+				n=w;
+			}
+		}
+
 		k=strlen(t);
 		h=m-n+1-k;
 		for (i=m+1; i<l+1; i++)
@@ -261,6 +266,13 @@ int bracket (char s[])
 	n=scan(s,'(',1,0);
 	unar(s,a);
 	return 1;
+}
+
+
+void end (char s[])
+{
+	double x=atof(s);
+	sprintf(s,"%.15g",x);
 }
 
 
@@ -330,164 +342,23 @@ void unar (char s[], char k[2])
 	}
 }
 
-
-void func (char s[])
+void func (char s[], int init, int fin, char t[])
 {
 	int i,l,m,n,k=0,v,u;
-	char t[N],c;
-	double x;
-	l=strlen(s);
-	m=scan(s,'(',0,0);
-
-	while (m>-1)
+	double x=atof(t);
+	for (i=init; i<=fin; i++)
+		t[k++]=s[i];
+	t[k]='\0';
+	for (i=0; i<fun_count; i++)
 	{
-		v=m;
-		if ((s[m-1]>='a' && s[m-1]<='z' ) || (s[m-1]>='0' && s[m-1]<='9'))
-		{
-			s[m]='[';
-			i=1; v=m;
-			while (i>0)
-			{
-				v++;
-				if 	(s[v]==')') i--;
-				else if (s[v]=='(') i++;
-			}
-			s[v]=']';
-
-			v=m;
-			do v--;
-			while (s[v-1]>='a' && s[v-1]<='z');
-
-			u=m-v;
-			switch (u)
-			{
-				case 2:
-					if (s[v]=='l' && s[v+1]=='n')
-						funcs(s,ln_X);
-					break;
-
-				case 3:
-					if (s[v]=='s' && s[v+1]=='i' && s[v+2]=='n')
-						funcs(s,sin_X);
-					else if (s[v]=='c' && s[v+1]=='o' && s[v+2]=='s')
-						funcs(s,cos_X);
-					else if (s[v]=='t' && s[v+1]=='a' && s[v+2]=='n')
-						funcs(s,tan_X);
-					else if (s[v]=='e' && s[v+1]=='x' && s[v+2]=='p')
-						funcs(s,exp_X);
-					break;
-
-				case 4:
-					if (s[v]=='a')
-					{
-						if (s[v+1]=='s' && s[v+2]=='i' && s[v+3]=='n')
-							funcs(s,asin_X);
-						else if (s[v+1]=='c' && s[v+2]=='o' && s[v+3]=='s')
-							funcs(s,acos_X);
-						else if (s[v+1]=='t' && s[v+2]=='a' && s[v+3]=='n')
-							funcs(s,atan_X);
-					}
-					else if (s[v+3]=='h')
-					{
-						if (s[v]=='s' && s[v+1]=='i' && s[v+2]=='n')
-							funcs(s,sinh_X);
-						else if (s[v]=='c' && s[v+1]=='o' && s[v+2]=='s')
-							funcs(s,cosh_X);
-						else if (s[v]=='t' && s[v+1]=='a' && s[v+2]=='n')
-							funcs(s,tanh_X);
-					}
-					else if (s[v]=='s' && s[v+1]=='q' && s[v+2]=='r' && s[v+3]=='t')
-						funcs(s,sqrt_X);
-					break;
-			}
-		}
-		else k=m-1;
-
-		m=scan(s,'(',0,k);
-	}
-}
-
-
-
-void funcs (char s[], int Flag)
-{
-	int i,m,n,l;
-	char t[N];
-	double x,y;
-	m=scan(s,'[',0,0);
-	n=scan(s,']',0,0);
-	l=n-m-1;
-
-	for (i=0; i<l; i++)
-		t[i]=s[i+m+1];
-	t[i]='\0';
-	calc(t);
-	x=atof(t);
-
-	switch (Flag)
-	{
-		case ln_X:
-			y=log(x);
+		if (strcmp(fun_list[i].name,t)==0)
 			break;
-
-		case sin_X:
-			y=sin(x);
-			break;
-
-		case cos_X:
-			y=cos(x);
-			break;
-
-		case tan_X:
-			y=tan(x);
-			break;
-
-		case exp_X:
-			y=exp(x);
-			break;
-
-		case log_X:
-			y=log(x);
-			break;
-
-		case asin_X:
-			y=asin(x);
-			break;
-
-		case acos_X:
-			y=acos(x);
-			break;
-
-		case atan_X:
-			y=atan(x);
-			break;
-
-		case sinh_X:
-			y=sinh(x);
-			break;
-
-		case cosh_X:
-			y=cosh(x);
-			break;
-
-		case tanh_X:
-			y=tanh(x);
-			break;
-
-		case sqrt_X:
-			y=sqrt(x);
-			break;
-
-		default:
-			return;
 	}
 
-	sprintf(t,"%.15g",y);
-	Flag=(int) Flag/1000;
-	m-=Flag;
-	s[m]='(';
-	s[n]=')';
-	fill(s,t,m+1,n-1);
+	//if i==fun_count -> error: undefined function 
+
+	x=fun_list[i].f(x);
+	sprintf(t,"%.15g",x);
 }
 
 
@@ -510,16 +381,23 @@ int solve (char s[], char k)
 		}
 		m=scan(s,k,1,0);
 
-		if ( s[0]=='-') m=scan(s,k,0,0);
-
+		if (s[0]=='-') 
+			m=scan(s,k,0,0);
 		if (m<1) break;
 
+		if (k=='-' && s[m-1]=='e')
+			break;
+
 		for (i=m-1; i>-1; i--)
-			if (scan(str,s[i],1,0)<0)
+		{
+
+			if (i>0 && s[i-1]=='e') i-=2;
+			else if (scan(str,s[i],1,0)<0)
 			{
 				if ((s[i]=='+' || s[i]=='-') && i>-1 && k!='^') i--;
 				break;
 			}
+		}
 
 		j=m+1;
 		if (s[j]=='+')
@@ -529,9 +407,15 @@ int solve (char s[], char k)
 				s[l]=s[l+1];
 			s[l]='\0';
 		}
+
 		else if (s[j]=='-') j++;
 
-		while (scan(str,s[j],1,0)>=0 && j<n+1)  j++;
+		while (scan(str,s[j],1,0)>=0 && j<n+1)
+		{
+			j++;
+			if (s[j]=='e') j+=2;
+		}
+
 
 		for (l=0; l<m-i-1; l++)
 			str1[l]=s[l+i+1];
@@ -597,18 +481,18 @@ int solve (char s[], char k)
 }
 
 
-int scan (char s[], char c, char d, int r)
+int scan (char s[], char c, char mode, int r)
 {
 	int i,n;
 	n=strlen(s);
-	if (d)
+	if (mode)
 	{
 		for (i=r; i<n; i++)
 			if (s[i]==c) return i;
 	}
 	else
 	{
-		if (!r) r=n-1;
+		if (r==0) r=n-1;
 		for (i=r; i>-1; i--)
 			if (s[i]==c) return i;
 	}
@@ -670,7 +554,18 @@ int fill (char s[], char r[], int a, int b)
 }
 
 
-int get(char * s)
+void insert (char s[], char c, int k)
+{
+	int i,n;
+	n=strlen(s);
+	for (i=n; i>k; i--)
+		s[i]=s[i-1];
+	s[k]=c;
+	s[n+1]='\0';
+}
+
+
+int get (char * s)
 {
 	char c,t[N],use=1;
 	int siz=0,i,j,k,cur_hor=0,cur_ver=line;
@@ -735,7 +630,7 @@ int get(char * s)
 							siz=cur_hor;
 						}
 						else
-						fputs("\a",stdout);
+							fputs("\a",stdout);
 						break;
 					case 'B': //Arrow Down
 						if (cur_ver<line)
@@ -754,7 +649,7 @@ int get(char * s)
 							siz=cur_hor;
 						}
 						else
-						fputs("\a",stdout);
+							fputs("\a",stdout);
 						break;
 					case 'C': //Arrow Right
 						if (cur_hor<siz)
@@ -814,8 +709,7 @@ char num (char c)
 {
 	if (c>='0' && c<='9')
 		return 1;
-	else
-		return 0;
+	return 0;
 }
 
 
@@ -823,8 +717,7 @@ char chara (char c)
 {
 	if (c>='a' && c<='z') 
 		return 1;
-	else
-		return 0;
+	return 0;
 }
 
 
@@ -832,25 +725,13 @@ char sign (char c)
 {
 	if (c=='+' || c=='-' || c=='*' || c=='/' || c=='^' || c=='<') 
 		return 1;
-	else
-		return 0;
+	return 0;
 }
 
 
-void Init()
+char bra (char c)
 {
-	logs=malloc(100*sizeof(char *));
-	var_list = malloc(100*sizeof(var));
-	fun_list = malloc(30*sizeof(vfun));
-	var a={"ans",0.0};
-	var b={"pi",3.14159265358979};
-	var_list[0]=a;
-	var_list[1]=b;
-}
-
-void Clear()
-{
-	free(logs);
-	free(var_list);
-	free(fun_list);
+	if (c=='(' || c==')')
+		return 1;
+	return 0;
 }
